dbc357fac0ac5bb6dcd486754f39b4aaa68058b7
Sarwat Haroon <159713603+Sarwatharoon68@users.noreply.github.com>
2024-04-26 09:47:18 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+
+let todos: string[] = ["eating", "drinking", "sleeping"];
+
+async function createTodo() {
+   do{  
+      let ans = await inquirer.prompt({
+      type: "list",
+      message: "Select an Operation",
+      name: "Select",
+      choices: ["Add", "Update", "View", "Delete"],
+  });
+
+  if (ans.Select === "Add") {
+      let addTodo = await inquirer.prompt({
+          type: "input",
+          message: "Add items in the list",
+          name: "Todo",
+      });
+      todos.push(addTodo.Todo);
+  }
+
+  if (ans.Select === "Update") {
+      let updateTodo = await inquirer.prompt({
+          type: "list",
+          message: "Update items in the list",
+          name: "Todo",
+          choices: todos,
+      });
+      let addTodo = await inquirer.prompt({
+          type: "input",
+          message: "Add items in the list",
+          name: "Todo",
+      });
+      let newTodo = todos.filter(val => val !== updateTodo.Todo);
+      todos = [...newTodo, addTodo.Todo];
+  }
+
+  if (ans.Select === "View") {
+      console.log(chalk.italic.blueBright("**** To Do List****"));
+      console.log(todos);
+      console.log(chalk.magenta("       ^^*************^^     "));
+  }
+
+  if (ans.Select === "Delete") {
+      let deleteTodo = await inquirer.prompt({
+          type: "list",
+          message: "Delete items in the list",
+          name: "Todo",
+          choices: todos,
+      });
+      todos = todos.filter(val => val !== deleteTodo.Todo);
+  }
+}
+ while(true)
+ }
+createTodo();
